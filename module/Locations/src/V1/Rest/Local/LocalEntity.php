f69ddced485174d1cc81d6108ad7c04cba2fe2f7
Bruno Menslin <brunomenslin@outlook.com>
2021-03-05 10:40:27 -0300

    feat: implement Local entity

--- module/Locations/src/V1/Rest/Local/LocalEntity.php
+++ module/Locations/src/V1/Rest/Local/LocalEntity.php
@@ -3,4 +3,23 @@ namespace Locations\V1\Rest\Local;
 
 class LocalEntity
 {
+    public $id;
+    public $name;
+    public $type_id;
+    
+    public function exchangeArray(array $data)
+    {
+        $this->id = $data('id');
+        $this->name = $data('name');
+        $this->type_id = $data('type_id');
+    }
+    
+    public function getArrayCopy()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'type_id' => $this->type_id,
+        ];
+    }    
 }
